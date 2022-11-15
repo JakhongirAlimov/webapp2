3ee5a18fa30c28d3a84811c2eed2647c1a1b7de5
spaces <34057581+JakhongirAlimov@users.noreply.github.com>
2022-11-14 22:17:11 -0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+	return "welcome!"
+
+@app.route('how are you')
+def helo():
+	return 'I am good, how  about u?'
+
+	if __name__ == "__main__":
+		app.run(host="0.0.0.0", port=8080)
